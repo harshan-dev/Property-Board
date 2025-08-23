bec6ecd415e128d1b4ac2207f246799bda201c6c
Sumith Harshan <sumithdev.com@gmail.com>
2025-08-23 10:54:23 +0530

    Change Navbar background color from blue to gray

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white shadow-lg">
+    <nav className="bg-gray-900 text-white shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
